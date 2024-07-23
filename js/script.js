65dc3b9c9feae09b00640a2775e848d582c96249
StanislavRex <dereviankostasian5@gmail.com>
2024-07-23 17:08:30 +0300

    Update CSS, add images, and modify landing.html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-import Swiper from "swiper/bundle";
-import "swiper/css/bundle";
 
-const swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
+function tabs(t) {
+  const e = document.querySelectorAll(t + " [data-tab]");
+  const a = document.querySelectorAll(t + " [data-tab-content]");
+
+  e.forEach(function (t) {
+    t.addEventListener("click", function (c) {
+      if (!t.classList.contains("active")) { 
+        e.forEach(function (t) {
+          t.classList.remove("active");
+        });
+        t.classList.add("active");
+
+        a.forEach(function (t) {
+          t.classList.remove("active");
+        });
+        document
+          .querySelector("#" + c.target.dataset.tab)
+          .classList.add("active");
+      }
+    });
+  });
+}
+
+tabs(".tab1");
+
+const swiper = new Swiper(".swiper", {
+  effect: "flip",
+  grabCursor: true,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
+    el: ".swiper-navigation",
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
