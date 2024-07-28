0e8ee33fb14ccb6525e9e65b19beb8ddee52693a
StanislavRex <dereviankostasian5@gmail.com>
2024-07-28 21:20:08 +0300

    Update CSS, add images, and modify landing.html

--- js/script.js
+++ js/script.js
@@ -5,3 +5,42 @@ const swiper = new Swiper(".heroSlider", {
 		prevEl: ".heroSliderNext",
 	},
 });
+
+const swiper2 = new Swiper(".swiper-2",{
+    direction: "horizontal",
+    loop: !0,
+    pagination: {
+        el: ".swiper-pagination2",
+        clickable: !0,
+        renderBullet: function(n, e) {
+            return '<span class="' + e + '"></span>'
+        }
+    },
+    noSwiping: !0,
+    noSwipingClass: "noswip",
+    navigation: {
+        nextEl: ".swiper-nav__rightbtn",
+        prevEl: ".swiper-nav__leftbtn"
+    }
+})
+
+const tabsNavAll = document.querySelectorAll("[data-tab]");
+const tabContentAll = document.querySelectorAll("[data-tab-content]");
+
+tabsNavAll.forEach(function(item){
+    item.addEventListener('click', function(event){
+        if(item.classList.contains("active")) return;
+        
+        tabsNavAll.forEach(function(i){
+            i.classList.remove('active');
+        });
+
+        item.classList.add('active');
+
+        tabContentAll.forEach(function(i){
+            i.classList.remove('active');
+        });
+
+        document.querySelector('#' + event.target.dataset.tab).classList.add('active');
+    })
+})
